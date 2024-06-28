90bc44f5a60287b4e39ba81cd4e738b9b761270f
Asghar Nazir <asghar@syntaxtechs.com>
2024-06-27 22:03:35 -0400

    SQL Utility

--- src/main/java/org/example/Review1.java
+++ src/main/java/org/example/Review1.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Review {
+public class Review1 {
     public static void main(String[] args) {
         String dbURL="jdbc:mysql://3.239.253.255:3306/syntaxhrm_mysql";
         String userName="syntax_hrm";
@@ -15,16 +19,19 @@ public class Review {
             ResultSet rs= statement.executeQuery(query);
             //next() it checks if there are any rows in the RS
             ResultSetMetaData rsm= rs.getMetaData();
-            for (int i = 1; i <= rsm.getColumnCount() ; i++) {
-                System.out.print(rsm.getColumnLabel(i)+" ");
-            }
-            System.out.println();
+            List<Map<String,String>> tableList=new ArrayList<>();
+
           while (rs.next()){
+              Map<String,String> row=new HashMap<>();
               for (int i = 1; i <= rsm.getColumnCount() ; i++) {
-                  System.out.print(rs.getString(i)+" ");
+                  String key=rsm.getColumnLabel(i);
+                  String value=rs.getString(i);
+                  row.put(key,value);
               }
-              System.out.println();
+              tableList.add(row);
+
           }
+            System.out.println(tableList);
 
         } catch (SQLException e) {
            e.printStackTrace();
